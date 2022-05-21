06ec07cfc072fde0c32f3e5272d095152fc564c4
Sergey Kuklin <ksv.crowley@gmail.com>
2022-05-21 21:54:02 +0500

    feat: Changes gql client

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,26 +1,27 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { GraphQLClient } from 'graphql-request';
 
-const httpLink = createHttpLink({
-  uri: process.env.GITHUB_API_URL
-});
+const getEndpoint = () => {
+  const url = process.env.GITHUB_API_URL;
 
-const authLink = setContext((_, { headers }) => {
+  if (url === undefined) {
+    throw Error('GITHUB_API_URL environment variable was not set');
+  }
+
+  return url;
+};
+
+const getToken = () => {
   const token = process.env.GITHUB_AUTH_TOKEN;
 
   if (token === undefined) {
     throw Error('GITHUB_AUTH_TOKEN environment variable was not set');
   }
 
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ''
-    }
-  };
-});
+  return token;
+};
 
-export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+export const client = new GraphQLClient(getEndpoint(), {
+  headers: {
+    authorization: `Bearer ${getToken()}`
+  }
 });

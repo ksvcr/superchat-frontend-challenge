8d577db61d2ca1e896dd2bf0aaa9e8c8bfe168eb
Sergey Kuklin <ksv.crowley@gmail.com>
2022-05-22 00:00:52 +0500

    feat: Extract TextInput

---
+++ src/shared/components/TextInput/TextInput.tsx
@@ -0,0 +1,16 @@
+import { InputHTMLAttributes } from 'react';
+
+type TextInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'type' | 'className'>;
+
+export const TextInput = ({ value, id, onChange, ...props }: TextInputProps) => {
+  return (
+    <input
+      type="text"
+      className="shadow border rounded w-full py-2 px-3 text-gray-700"
+      value={value}
+      id={id}
+      onChange={onChange}
+      {...props}
+    />
+  );
+};

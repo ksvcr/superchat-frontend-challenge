4789b2b26ef32d3be94f95a039c25996ccd49381
Sergey Kuklin <ksv.crowley@gmail.com>
2022-05-23 13:47:46 +0500

    fix: Removes unused dark

--- src/shared/components/Spinner/Spinner.tsx
+++ src/shared/components/Spinner/Spinner.tsx
@@ -16,7 +16,7 @@ const getSizes = (size: string): string => {
 };
 
 export const Spinner = ({ size = 'md' }: SpinnerProps) => {
-  const className = `inline ${getSizes(size)} mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600`;
+  const className = `inline ${getSizes(size)} mr-2 text-gray-200 animate-spin fill-blue-600`;
 
   return (
     <svg role="status" className={className} viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
